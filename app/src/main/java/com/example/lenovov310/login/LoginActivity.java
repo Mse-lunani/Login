b888d804fc28c22065123847fa484c2a9d62ffab
Mse-lunani <35915767+Mse-lunani@users.noreply.github.com>
2018-07-09 14:44:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lenovov310/login/LoginActivity.java
@@ -0,0 +1,127 @@
+package com.example.lenovov310.login;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity  {
+
+    public void OnLogin(View view) {
+        String username = Email.getText().toString();
+        String password = Password.getText().toString();
+        String type = "login";
+        UserLoginTask backgroundWorker = new UserLoginTask(this);
+        backgroundWorker.execute(type, username, password);
+    }
+
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private AutoCompleteTextView Email;
+    private EditText Password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        Email = (AutoCompleteTextView) findViewById(R.id.email);
+
+
+        Password = (EditText) findViewById(R.id.password);}
+
+    public class UserLoginTask extends AsyncTask<String, Void, String> {
+        String mEmail;
+        String mPassword;
+        Context context;
+        AlertDialog alertDialog;
+        UserLoginTask (Context ctx) {
+            context = ctx;
+        }
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String type = params[0];
+            String login_url = "http://login.ruitech.co.ke/login.php";
+            if(type.equals("login")) {
+                try {
+                    String user_name = params[1];
+                    String password = params[2];
+                    URL url = new URL(login_url);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                    String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                            +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                    bufferedWriter.write(post_data);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                    String result="";
+                    String line="";
+                    while((line = bufferedReader.readLine())!= null) {
+                        result += line;
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    return result;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            alertDialog = new AlertDialog.Builder(context).create();
+            alertDialog.setTitle("Login Status");
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            alertDialog.setMessage(result);
+            alertDialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+    }
+}
+
